9110e98a8c0fd2e70c0474e5f3dd2a10f9249bdd
Drew Herren <drewherrenopensource@gmail.com>
2025-02-07 10:00:02 +0000

    version 0.1.0

---
+++ R/stochtree-package.R
@@ -0,0 +1,17 @@
+## usethis namespace: start
+#' @importFrom stats coef
+#' @importFrom stats lm
+#' @importFrom stats model.matrix
+#' @importFrom stats predict
+#' @importFrom stats qgamma
+#' @importFrom stats resid
+#' @importFrom stats rnorm
+#' @importFrom stats sd
+#' @importFrom stats sigma
+#' @importFrom stats var
+#' @importFrom R6 R6Class
+## usethis namespace: end
+NULL
+
+#' @useDynLib stochtree, .registration = TRUE
+"_PACKAGE"
